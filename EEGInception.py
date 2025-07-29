b668214854194466fef40a25142f41b05d2403fc
Salvatore <slvtrferrara@gmail.com>
2025-07-29 23:45:27 +0200

    EEGInception commented

--- EEGInception.py
+++ EEGInception.py
@@ -84,114 +84,176 @@ class EEGInception(nn.Module):
         self.conv2 = nn.Conv2d(3 * n_filters, 3 * n_spatial * n_filters,
                                kernel_size=(in_shape[-2], 1),
                                bias=False, groups=3 * n_filters)
+        # strato che applica filtri spaziali.
+        # kernel_size=(in_shape[-2], 1) significa che la convoluzione opera su tutti i canali EEG
+        # (in_shape [-2] è il numero di canali) combinandoli in nuove feature spaziali. 
+        # groups=3 * n_filters indica una convoluzione a profondità separabile (depthwise separable convolution)
+        # dove i filtri sono applicati indipendentemente per ciascun gruppo di canali di input. Questo
+        # un modo efficente per catturare le relazioni spaziali.
+
         if alignment == 'latent':
             self.latent_align2 = LatentAlignment2d(self.conv2.out_channels)
         elif alignment == 'adaptive':
             self.abn2 = AdaptiveBatchNorm2d(self.conv2.out_channels)
         else:
             self.bn2 = nn.BatchNorm2d(self.conv2.out_channels)
-        self.drop2 = nn.Dropout(dropout)
-        self.pool1 = nn.AvgPool2d(kernel_size=(1, 4))
 
-        # Inception 2
+        # strato di allineamento/normalizzazione
+        self.drop2 = nn.Dropout(dropout)
+        # strato di dropout
+        self.pool1 = nn.AvgPool2d(kernel_size=(1, 4))
+        # strato di average pooling per ridurre la dimensione temporale
+
+        # Blocco Inception 2
+        # secondo modulo simile al primo, ma operante sulle feature ma estratte dal blocco precedente.
+        # Utilizza dimensioni di kernel più piccole (13,7,3)
         self.conv3a = nn.Conv2d(self.conv2.out_channels, self.conv2.out_channels,
                                 kernel_size=(1, 13), padding=(0, 6), bias=False)
         self.conv3b = nn.Conv2d(self.conv2.out_channels, self.conv2.out_channels,
                                 kernel_size=(1, 7), padding=(0, 3), bias=False)
         self.conv3c = nn.Conv2d(self.conv2.out_channels, self.conv2.out_channels,
                                 kernel_size=(1, 3), padding=(0, 1), bias=False)
+        # 3 convoluzioni in parallelo
         if alignment == 'latent':
             self.latent_align3 = LatentAlignment2d(3 * self.conv2.out_channels)
         elif alignment == 'adaptive':
             self.abn3 = AdaptiveBatchNorm2d(3 * self.conv2.out_channels)
         else:
             self.bn3 = nn.BatchNorm2d(3 * self.conv2.out_channels)
+        # strato di allineamento/normalizzazione
         self.drop3 = nn.Dropout(dropout)
+        # strato di dropout
         self.pool2 = nn.AvgPool2d(kernel_size=(1, 2))
+        # Secondo strato di average pooling
 
-        # Separable Convolution 1
+        # Convoluzioni Separabili
+        # due ulteriori strati convoluzionali, probabilmente per affinare ulteriormente le feature. 
+        # Anche qui, l'allineamento/normalizzazione e il pooling vengono applicati dopo ciascuno
         self.conv4 = nn.Conv2d(3 * self.conv2.out_channels, 3 * self.conv2.out_channels,
                                kernel_size=(1, 7), padding=(0, 3), bias=False)
+        # strato di convoluzione con kernel_size=(1, 7).
+
         if alignment == 'latent':
             self.latent_align4 = LatentAlignment2d(self.conv4.out_channels)
         elif alignment == 'adaptive':
             self.abn4 = AdaptiveBatchNorm2d(self.conv4.out_channels)
         else:
             self.bn4 = nn.BatchNorm2d(self.conv4.out_channels)
-        self.pool3 = nn.AvgPool2d(kernel_size=(1, 2))
-        self.drop4 = nn.Dropout(dropout)
+        # strato di normalizzazione
 
-        # Separable Convolution 2
+        self.pool3 = nn.AvgPool2d(kernel_size=(1, 2))
+        # strato di average pooling
+
+        self.drop4 = nn.Dropout(dropout)
+        # strato di dropout
+        
         self.conv5 = nn.Conv2d(self.conv4.out_channels, self.conv4.out_channels,
                                kernel_size=(1, 3), padding=(0, 1), bias=False)
+        # strato di convoluzione con e kernel_size=(1, 3).
+
         if alignment == 'latent':
             self.latent_align5 = LatentAlignment2d(self.conv5.out_channels)
         elif alignment == 'adaptive':
             self.abn5 = AdaptiveBatchNorm2d(self.conv5.out_channels)
         else:
             self.bn5 = nn.BatchNorm2d(self.conv5.out_channels)
+         # strato di normalizzazione
+
         self.pool4 = nn.AvgPool2d(kernel_size=(1, 2))
+        # strato di average pooling
+
         self.drop5 = nn.Dropout(dropout)
+        # strato di dropout
 
-        # Classifier
+        # Classificatore
         self.n_features = self.conv5.out_channels * int(np.floor(in_shape[-1] / 4 / 2 / 2 / 2))
-        self.fc_out = nn.Linear(self.n_features, n_out)
+        # calcola il numero di feature appiattite in base alle dimensioni di output finali dopo tutte
+        # le convoluzioni e i pooling. L'uso di np.floor indica che si gestiscono divisioni intere.
 
+        self.fc_out = nn.Linear(self.n_features, n_out)
+        # strato fully connected finale per la classificazione.
+
+
+    # metodo forward che definisce il flusso dei dati attraverso l'architettura
     def forward(self, x, sbj_trials):
         """
         Args:
-             x: (batch * sbj_trials, spatial, time)
-             sbj_trials: number of trials per subject
+             x: tensore di input, con forma (batch * sbj_trials, spatial, time)
+             sbj_trials: numero di prove per ogni soggetto nel batch.
         """
         _, spatial, time = x.shape
 
-        # Euclidean alignment
+        # Allineamento euclideo
         if self.alignment == 'euclidean':
             x = self.euclidean(x, sbj_trials)
+            # viene applicato sui dati di input grezzi
 
-        # Input alignment
+        # Allineamento Input
         x = x.reshape(-1, spatial, 1, time)
+        # l'input viene rimodellato per raggiungere la dimensione del canale (1) necessaria per gli strati Conv2d di PyTotch
+        
         if self.alignment == 'latent':
             x = self.latent_align0(x, sbj_trials)
         elif self.alignment == 'adaptive':
             x = self.abn0(x, sbj_trials)
         else:
             x = self.bn0(x)
+        # viene applicato lo strato di normalizzazione dell'input
         x = x.reshape(-1, spatial, time)
-
-        # Create artificial image dimension
+        # l'input viene rimodellato per rimuovere la dimensione del canale artificiale
+    
         x = x.unsqueeze(1)
+        # la dimensione del canale viene aggiunta di nuovo, questa volta per il modulo Inception che segue
 
         # Inception 1
         x1 = self.conv1a(x)
         x2 = self.conv1b(x)
         x3 = self.conv1c(x)
+        # l'input viene passato attraverso i tre rami convoluzionali paralleli
+
         x = torch.cat((x1, x2, x3), dim=1)
+        # i risultati dei tre rami vengono concatenati lungo la dimensione dei canali
+
         if self.alignment == 'latent':
             x = self.latent_align1(x, sbj_trials)
         elif self.alignment == 'adaptive':
             x = self.abn1(x, sbj_trials)
         else:
             x = self.bn1(x)
+        # strato di allineamento/normalizzazione
+        
         x = F.elu(x)
+        # di attivazione F.elu (Exponetial Linear Unit)
+        
         x = self.drop1(x)
+        # e il dropout
 
-        # Spatial Filters
+        # Filtri spaziali
         x = self.conv2(x)
+        # applica i filtri spaziali
+
         if self.alignment == 'latent':
             x = self.latent_align2(x, sbj_trials)
         elif self.alignment == 'adaptive':
             x = self.abn2(x, sbj_trials)
         else:
             x = self.bn2(x)
+        # allineamento/normalizzaizione
+        
         x = F.elu(x)
+        # attivazione
+        
         x = self.drop2(x)
+        #dropout
+        
         x = self.pool1(x)
+        # average pooling
 
-        # Inception 2
+        # Inception 2 simile al blocco inception 1
         x1 = self.conv3a(x)
         x2 = self.conv3b(x)
         x3 = self.conv3c(x)
+
         x = torch.cat((x1, x2, x3), dim=1)
         if self.alignment == 'latent':
             x = self.latent_align3(x, sbj_trials)
@@ -203,19 +265,28 @@ class EEGInception(nn.Module):
         x = self.drop3(x)
         x = self.pool2(x)
 
-        # Separable Convolution 1
+        # Convoluzione Separabile 1
         x = self.conv4(x)
+        # prima convoluzione separabile
+
         if self.alignment == 'latent':
             x = self.latent_align4(x, sbj_trials)
         elif self.alignment == 'adaptive':
             x = self.abn4(x, sbj_trials)
         else:
             x = self.bn4(x)
-        x = F.elu(x)
-        x = self.pool3(x)
-        x = self.drop4(x)
+        # allineamento/normalizzazione
 
-        # Separable Convolution 2
+        x = F.elu(x)
+        # attivazione
+
+        x = self.pool3(x)
+        # average pooling
+
+        x = self.drop4(x)
+        # dropout
+
+        # Convoluzione separabile 2
         x = self.conv5(x)
         if self.alignment == 'latent':
             x = self.latent_align5(x, sbj_trials)
@@ -227,8 +298,12 @@ class EEGInception(nn.Module):
         x = self.pool4(x)
         x = self.drop5(x)
 
-        # Classifier
+        # Classificatore Finale
         x = x.reshape(-1, self.n_features)
-        x = self.fc_out(x)
+        # le feature finali vengono appiattite
 
+        x = self.fc_out(x)
+        # le feature appiattite vengono passate allo strato lineare finale per
+        # produrre i logit di classificazione. Si noti che qui non c'è un F.relu
+        # prima dello strato lineare finale, il che è comune per i logit di output
         return x
